b12d3acb67749eaad039f18b2436197838b18848
miguelmoreira7 <miguelmoreiracr7@gmail.com>
2023-10-30 18:50:42 -0300

    commit inicial

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+import CustomButton from './CustomButton'
+import Hero from './Hero'
+
+export {
+    Navbar,
+    Footer,
+    CustomButton,
+    Hero,
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Hero } from "../components"
+
+
+const Home = () => {
+  return (
+    <div className="overflow-hidden">
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home
