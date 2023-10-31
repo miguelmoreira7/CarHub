dbe7c8efe3cfc07f5f1915f02998fa7e195223dd
miguelmoreira7 <miguelmoreiracr7@gmail.com>
2023-10-30 23:52:33 -0300

    footer terminado, início do desenvolvimento do combobox e da comunicação com a api

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,10 +2,16 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import CustomButton from './CustomButton'
 import Hero from './Hero'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import SearchManufacturer from './SearchManufacturer'
 
 export {
     Navbar,
     Footer,
     CustomButton,
     Hero,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
 }
